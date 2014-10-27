59b3e000426d01fedf6d31fbafc2cc6e30796ff6
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2014-10-27 15:03:05 +0100

    New footer css.

--- action.js
+++ action.js
@@ -4,8 +4,8 @@ $( document ).ready(function() {
 	var stopWords = /^(i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$/;
 	var fill = d3.scale.category20(); //colors
 	var fontSize = d3.scale.log().range([15, 105]);
-	var width = 960;
-	var height = 600;
+	var width = 900;
+	var height = 500;
 
 	var URLhash = location.hash.replace('#', '');
 	var ArtistAndSong = URLhash || randomPropertyName(cache);
@@ -117,7 +117,7 @@ $( document ).ready(function() {
 				.data(words)
 			.enter().append("text")
 				.style("font-size", function(d) { return d.size + "px"; })
-				.style("font-family", "Times New Roman")
+				.style("font-family", "serif")
 				.style("fill", function(d, i) { return fill(i); })
 				.attr("text-anchor", "middle")
 				.attr("transform", function(d) {
